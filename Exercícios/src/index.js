fa10854777ad2d3dba731bdad084e7ec5ef426c1
isadoramsouza <isadora_msouza@hotmal.com>
2018-11-26 21:16:08 -0200

    exercício 11

--- Exercícios/src/index.js
+++ Exercícios/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import ClassComponent from './classComponent'
 
 ReactDOM.render(
-    <ClassComponent value='Componente de Classe'/>
+    <ClassComponent label='Contador' initialValue={10}/>
 , document.getElementById('app'))
 
 
