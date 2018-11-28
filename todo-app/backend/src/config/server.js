fc5f52d12e11eb55e52c9f1f67367393ce7e56e7
isadoramsouza <isadora_msouza@hotmal.com>
2018-11-28 21:11:13 -0200

    backend concluído

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -1,12 +1,17 @@
 const port = 3003
+
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors= require('./cors')
 
-server.use(bodyParser.urlencoded({extendes: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
-server.listen(port, function(){
-    console.log(`backend is running in port ${port}`)
-    
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
+
